9b57b59b0fa17e82ea443f15e58f031517ff6302
Matheus Oliveira <cruz.matheus99@gmail.com>
2022-06-09 17:06:24 -0300

    Initial Commit

---
+++ CampeoesDaCopa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Matheus Oliveira on 09/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
